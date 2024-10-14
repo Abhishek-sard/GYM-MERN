77d1b6b5d4c07c9aba2b361dcb4e16a6fa4ac1f9
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-10-14 15:57:34 +0545

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import { Router, BrowserRouter as router } from 'react-router-dom';
+import './App.css'
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import WorkoutSessions from './Components/WorkoutSessions';
+import Gallery from './Components/Gallery';
+import Pricing from './Components/Pricing';
+import Contact from './Components/Contact';
+import BMICalculator from './Components/BMICalculator';
+import Footer from './Components/Footer';
 
 
 const App = () => {
